1b37966f81d71320c2dbc3f50198d63ddcb63691
Juan Gallegos <juangallegos@mac.com>
2021-01-21 12:07:12 -0600

    Modified dataimporter.py to make DataImporter class. The function import data was moved to DataImporter method.

--- store_inventory/dataimport.py
+++ store_inventory/dataimport.py
@@ -1,14 +1,22 @@
 import csv
 
 
-def readfile():
-    with open('inventory.csv', newline='') as csvfile:
-        prodreader = csv.DictReader(csvfile, delimiter=',')
-        rows = list(prodreader)
+class DataImporter:
+    def __init__(self):
+        pass
+
+    def readfile(self):
+        with open('inventory.csv', newline='') as csvfile:
+            prodreader = csv.DictReader(csvfile, delimiter=',')
+            rows = list(prodreader)
+            return rows
+
+    def import_data(self):
+        rows = self.readfile()
         return rows
 
 
 if __name__ == '__main__':
-    rows = readfile()
+    rows = DataImporter.readfile()
     for row in rows:
         print(row['product_name'])
