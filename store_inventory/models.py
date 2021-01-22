9daad95f0298b99870e7b84087ea1ed9039860bd
Juan Gallegos <juangallegos@mac.com>
2021-01-21 23:21:39 -0600

    Updated model.py to include database related methods previously found in app.py.

--- store_inventory/models.py
+++ store_inventory/models.py
@@ -3,6 +3,7 @@ from peewee import Model, PrimaryKeyField, CharField, IntegerField, DateTimeFiel
 
 db = SqliteDatabase('inventory.db')
 
+
 class Product(Model):
     product_id = PrimaryKeyField()
     product_name = CharField(max_length=255, unique=True)
@@ -12,3 +13,43 @@ class Product(Model):
 
     class Meta:
         database = db
+
+    def initialize(self):
+        """Create the database and table if they do not exist."""
+        db.connect()
+        db.create_tables([Product], safe=True)
+
+    def add_import_entry(self, product_name, product_price,
+                         product_quantity, date_updated):
+        """Add an entry"""
+        Product.create(product_name=product_name,
+                       product_price=product_price,
+                       product_quantity=product_quantity,
+                       date_updated=date_updated)
+        # print('Saved successfully!')
+
+    def get_product_by_name(self, name):
+        '''Returns products where product_name contains name'''
+        products = Product.select().order_by(Product.date_updated.desc())
+        products = products.where(Product.product_name.contains(name))
+        return products
+
+    def update_entry_using_query(self, product, price, quantity, updated):
+        '''
+        Query and retrieve an object by ID and then update product details
+        '''
+        pdetails = Product.select()
+        pdetails.where(Product.product_id == product.product_id)
+        pdetails.get()
+        pdetails.product_price = price
+        pdetails.product_quantity = quantity
+        pdetails.date_updated = updated
+        pdetails.save()
+
+    def update_entry(self, product, price, quantity, updated):
+        '''Use update statement to update product details where product_id'''
+        q = Product.update(product_price=price,
+                           product_quantity=quantity,
+                           date_updated=updated
+                           ).where(Product.product_id == product.product_id)
+        q.execute()
