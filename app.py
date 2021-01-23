64776ed4868a23d7ed144c3a1fcfcf107156d3f9
Juan Gallegos <juangallegos@mac.com>
2021-01-23 00:34:23 -0600

    Removes Menu().greeting as it now called within the menu_display method.

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-# import datetime
 from store_inventory.menu import Menu
 from store_inventory.models import Product
 from store_inventory.dataimport import DataImporter
 
-# from peewee import *
-
 
 def main():
     Product().initialize()
-    # dimporter = DataImporter()
-    # data = dimporter.import_data()
     data = DataImporter().import_data()
     DataImporter().clean_data(data)
-    Menu().greeting()
     Menu().menu_display()
 
 
